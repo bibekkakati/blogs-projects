85db59b1e981064266f39d680e717bc45566b2d5
bibekkakati <bibekkakati0@gmail.com>
2021-05-06 21:25:00 +0530

    mozVibrate

--- web/vibration-api/script.js
+++ web/vibration-api/script.js
@@ -1,7 +1,10 @@
-if (Boolean(window.navigator.vibrate)) {
+if (Boolean(window.navigator.vibrate) || Boolean(window.navigator.mozVibrate)) {
 	const singleVibration = document.getElementById("singleVibration");
 	const patternVibration = document.getElementById("patternVibration");
 	const cancelVibration = document.getElementById("cancelVibration");
+
+	window.navigator.vibrate =
+		window.navigator.vibrate || window.navigator.mozVibrate;
 
 	singleVibration.onclick = function (e) {
 		window.navigator.vibrate(500);
