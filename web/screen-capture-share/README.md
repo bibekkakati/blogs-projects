b4bfc864cc54e132facff4f0c8dccd5d4d40f7bf
bibekkakati <bibekkakati0@gmail.com>
2021-05-13 18:37:57 +0530

    Screen capture

---
+++ web/screen-capture-share/README.md
@@ -0,0 +1,1 @@
+### Capture Screen Like Zoom Using JavaScript
---
+++ web/screen-capture-share/script.js
@@ -0,0 +1,45 @@
+function main() {
+	const video = document.getElementById("video");
+	const start = document.getElementById("start");
+	const stop = document.getElementById("stop");
+
+	var displayMediaOptions = {
+		video: {
+			cursor: "always",
+		},
+		audio: false,
+	};
+
+	start.onclick = function (e) {
+		startSharing();
+	};
+
+	stop.onclick = function (e) {
+		stopSharing();
+	};
+
+	async function startSharing() {
+		let capture = null;
+		try {
+			capture = await window.navigator.mediaDevices.getDisplayMedia(
+				displayMediaOptions
+			);
+
+			capture
+				.getVideoTracks()[0]
+				.addEventListener("ended", () => stopSharing());
+
+			video.srcObject = capture;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	function stopSharing() {
+		let tracks = video.srcObject.getTracks();
+		tracks.forEach((track) => track.stop());
+		video.srcObject = null;
+	}
+}
+
+main();
