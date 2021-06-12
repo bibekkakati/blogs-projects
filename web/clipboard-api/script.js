4827b0ffbdc6e8bebb5767f183da8ae3fd01f1d3
bibekkakati <bibekkakati0@gmail.com>
2021-06-12 17:49:45 +0530

    Cut feature

--- web/clipboard-api/script.js
+++ web/clipboard-api/script.js
@@ -1,11 +1,24 @@
-const content = document.getElementById("content");
+const contentCp = document.getElementById("content-cp");
+const contentCt = document.getElementById("content-ct");
 const copyBtn = document.getElementById("copy");
+const cutBtn = document.getElementById("cut");
 
 copyBtn.onclick = copyToClipboard;
+cutBtn.onclick = cutToClipboard;
 
 function copyToClipboard() {
 	navigator.clipboard
-		.writeText(content.innerText)
+		.writeText(contentCp.innerText)
 		.then(() => alert("Copied to clipboard"))
+		.catch((e) => alert(e.message));
+}
+
+function cutToClipboard() {
+	navigator.clipboard
+		.writeText(contentCt.innerText)
+		.then(() => {
+			contentCt.innerText = "";
+			alert("Copied to clipboard");
+		})
 		.catch((e) => alert(e.message));
 }
